0ab97c7b7000041abb5bff22406e3631f3dff2b9
hanruobing <hanruobing@wecash.net>
2017-08-01 18:29:51 +0800

    add norm data func

--- read_data.py
+++ read_data.py
@@ -1,9 +1,6 @@
-import os
-import time
 def get_field_feature(file_name = './test_data.txt'):
     file = open(file_name)
     meta_data = file.readlines()
-    meta_data = meta_data[:2]
     file.close()
     '''
     get fields and features
@@ -17,3 +14,25 @@ def get_field_feature(file_name = './test_data.txt'):
             field_set.add(attrs[0])
             feature_set.add(attrs[1])
     return field_set,feature_set
+
+def normalize_data_file(field_dic,feature_dic,meta_file_name = './test_data.txt'):
+    file = open('./norm_test_data.txt','w')
+    for sample in open(meta_file_name):
+        attribute = sample.split()
+        str = attribute[0]
+        for value in attribute[1:]:
+            attrs = value.split(':')
+            norm_field = field_dic[attrs[0]]
+            norm_feature = feature_dic[attrs[1]]
+            str += ' {}:{}:{}'.format(norm_field,norm_feature,attrs[2])
+        file.write(str+'\n')
+    file.close()
+if __name__ == "__main__":
+    field_set,feature_set = get_field_feature()
+    field_dic = {}
+    for idx,item in enumerate(list(field_set)):
+        field_dic[item] = idx
+    feature_dic = {}
+    for idx,item in enumerate(list(feature_set)):
+        feature_dic[item] = idx
+    normalize_data_file(field_dic,feature_dic)
