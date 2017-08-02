57a7cbaf10bc3a9bca69ffb0327f39609769f570
hanruobing <hanruobing@wecash.net>
2017-08-02 15:58:35 +0800

    implement some func in FFM

--- FFM.py
+++ FFM.py
@@ -1,1 +1,30 @@
 import tensorflow as tf
+configure = {
+    'batch_size':64,
+    'learning_rate':0.001
+}
+data_path = './norm_test_data.txt'
+field_num = 10
+feature_num = 10
+feature_emb_size = 64
+field_emb_size = 64
+class FFM:
+    def __init__(self):
+        self.batch_size = configure['batch_size']
+        self.lr = configure['learning_rate']
+        self.data_path = data_path
+        self.field_num = field_num
+        self.feature_num = feature_num
+        with tf.name_scope('embedding_matrix'):
+            self.feature_embedding = tf.get_variable(name='feature_embedding',
+                                                shape=[feature_num,feature_emb_size],
+                                                dtype=tf.float32)
+            self.field_embedding = []
+            for idx in xrange(0,self.feature_num):
+                self.field_embedding.append(tf.get_variable(name='field_embedding{}'.format(idx),
+                                                            shape=[field_num,field_emb_size],
+                                                            dtype=tf.float32))
+        with tf.name_scope('input'):
+            self.label = tf.placeholder(tf.float32, shape=(self.batch_size))
+            self.feature_value = tf.placeholder(tf.float32,shape=(self.batch_size,feature_num))
+if __name__ == "__main__":
--- read_data.py
+++ read_data.py
@@ -1,3 +1,10 @@
+import os
+
+'''
+notice:feature id should be identity among all field,
+which means there should not be different feature in different fiedls
+that have the same feature id
+'''
 def get_field_feature(file_name = './test_data.txt'):
     file = open(file_name)
     meta_data = file.readlines()
@@ -15,19 +22,31 @@ def get_field_feature(file_name = './test_data.txt'):
             feature_set.add(attrs[1])
     return field_set,feature_set
 
+'''
+change feature and field id to range[0-N)
+'''
 def normalize_data_file(field_dic,feature_dic,meta_file_name = './test_data.txt'):
+    if os.path.exists('./norm_test_data.txt'):
+        print("norm dataset already exists")
+        return
     file = open('./norm_test_data.txt','w')
     for sample in open(meta_file_name):
         attribute = sample.split()
         str = attribute[0]
         for value in attribute[1:]:
             attrs = value.split(':')
+            if(float(attrs[2]) == 0):
+                #if feature's value is 0, then ignore that feature
+                #may be need normalize in future version
+                continue
             norm_field = field_dic[attrs[0]]
             norm_feature = feature_dic[attrs[1]]
             str += ' {}:{}:{}'.format(norm_field,norm_feature,attrs[2])
         file.write(str+'\n')
     file.close()
+
 if __name__ == "__main__":
+    print("get field&feature id")
     field_set,feature_set = get_field_feature()
     field_dic = {}
     for idx,item in enumerate(list(field_set)):
@@ -35,4 +54,5 @@ if __name__ == "__main__":
     feature_dic = {}
     for idx,item in enumerate(list(feature_set)):
         feature_dic[item] = idx
+    print("get norm dataset")
     normalize_data_file(field_dic,feature_dic)
