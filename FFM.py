53fc10d042863c5aa4e5e4940873d91ea9f26f82
hanruobing <hanruobing@wecash.net>
2017-08-03 17:00:56 +0800

    normally update

--- FFM.py
+++ FFM.py
@@ -1,20 +1,22 @@
 import tensorflow as tf
+import random
 configure = {
     'batch_size':64,
     'learning_rate':0.001
 }
 data_path = './norm_test_data.txt'
-field_num = 10
-feature_num = 10
-def prepare_data(file_path = './norm_test_data.txt'):
-    '''
-
+field_num = 0
+feature_num = 0
+def prepare_data(file_path):
+    """
     :param file_path:
     :return: a tuple (data_set,feature2field)
     data_set is a list,each element is a list,the last is label
-    '''
+    """
     feature2field = {}
     data_set = []
+    global field_num
+    global feature_num
     for sample in open(file_path,'r'):
         sample_data = []
         field_features = sample.split()[1:]
@@ -22,19 +24,27 @@ def prepare_data(file_path = './norm_test_data.txt'):
             feature = int(field_feature_pair.split(':')[1])
             field = int(field_feature_pair.split(':')[0])
             value = float(field_feature_pair.split(':')[0])
+            if(field + 1 > field_num):
+                field_num = field + 1
+            if(feature + 1 > feature_num):
+                feature_num = feature + 1
             feature2field[feature] = field
             sample_data.append('{}:{}'.format(feature,value))
         sample_data.append(int(sample[0]))
         data_set.append(sample_data)
     return data_set,feature2field
 class FFM:
-    def __init__(self, batch_size ,learning_rate,data_path,field_num,feature_num,feature2field):
+    def __init__(self, batch_size ,learning_rate,
+                 data_path,field_num,
+                 feature_num,feature2field,data_set):
         self.batch_size = batch_size
         self.lr = learning_rate
         self.data_path = data_path
         self.field_num = field_num
         self.feature_num = feature_num
         self.feature2field = feature2field
+        self.data_set = data_set
+        '''
         with tf.name_scope('embedding_matrix'):
             #a tensor of shape [feature_num] to hold each Wi
             self.liner_weight = tf.get_variable(name='line_weight',
@@ -43,10 +53,10 @@ class FFM:
                                                       initializer=tf.truncated_normal_initializer(stddev=0.01))
             self.field_embedding = []
             for idx in xrange(0,self.feature_num):
-                '''
-                a list or tensor which stores each feature's vector to each identity field,
-                shape = [feature_num * field_num]
-                '''
+                
+                #a list or tensor which stores each feature's vector to each identity field,
+                #shape = [feature_num * field_num]
+                
                 self.field_embedding.append(tf.get_variable(name='field_embedding{}'.format(idx),
                                                             shape=[field_num],
                                                             dtype=tf.float32,
@@ -60,13 +70,16 @@ class FFM:
                                        shape=(self.batch_size),
                                        name = 'feature_{}'.format(idx)))
         with tf.name_scope('network'):
-            '''
-            b0:constant bias
-            predict = b0 + sum(Vi * feature_i) + sum(Vij * Vji * feature_i * feature_j)
-            '''
+            
+            #b0:constant bias
+            #predict = b0 + sum(Vi * feature_i) + sum(Vij * Vji * feature_i * feature_j)
+            
             self.b0 = tf.get_variable(name='bias_0',shape=[1],dtype=tf.float32)
             #calculate liner term
-            self.liner_term = tf.reduce_sum(tf.multiply(tf.convert_to_tensor(self.feature_value),self.liner_weight))
+            self.liner_term = tf.reduce_sum(tf.multiply(tf.transpose(
+                                                            tf.convert_to_tensor(self.feature_value),
+                                                            perm=[1, 0])
+                                                        ,self.liner_weight))
             #calculate quadratic term
             self.qua_term = tf.get_variable(name='quad_term',shape=[1],dtype=tf.float32)
             for f1 in xrange(0,feature_num-1):
@@ -81,16 +94,37 @@ class FFM:
             self.opt = self.optimizer.apply_gradients(self.grad)
         self.sess = tf.InteractiveSession()
         self.sess.run(tf.global_variables_initializer())
-
-
-    def step(self,feature,label):
         '''
-        :param feature: shape[batch_size ,feature_num] each element is a sclar
-        :param label:[batch_size] each element is 0 or 1
+
+    def step(self):
+        '''
         :return: log_loss
         '''
-    def get_data(self,data_set):
+        feature,label = self.get_data()
+    def get_data(self):
         """
-        :return: a tuple of feature and x
+        :return: a tuple of feature and label
+        feature: shape[batch_size ,feature_num] each element is a sclar
+        label:[batch_size] each element is 0 or 1
         """
+        feature = []
+        label = []
+        for _ in xrange(0,self.batch_size):
+            t_feature = [0.0] * feature_num
+            sample = self.data_set[random.randint(0, len(self.data_set))]
+            label.append(sample[-1])
+            sample = sample[:-1]
+            for f in sample:
+                #print("f={}".format(f))
+                #print(f.split(':'))
+                t_feature[int(f.split(':')[0])] = float(f.split(':')[1])
+            feature.append(t_feature)
+        return feature,label
+
 if __name__ == "__main__":
+    data_set,feature_map = prepare_data(file_path=data_path)
+    print("feature num {} field num {}".format(feature_num,field_num))
+    ffm = FFM(4,0.01,data_path,field_num,feature_num,feature_map,data_set)
+    feature,label = ffm.get_data()
+    print(feature)
+    print(label)
